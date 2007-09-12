c8b48fcae07c8a3908fcd0d60ae6c962756c6c71
smckay <smckay>
2007-09-12 00:41:13 +0000

    making the site behave in IE < 7

--- html/js/modencode.js
+++ html/js/modencode.js
@@ -1,7 +1,53 @@
+
+if (isOldIE()) window.attachEvent("onload", correctPNG);
+
 function setActiveLink(section) {
     /* currently disabled for IE until I can
        get access to windows */
-    if (document.all && !window.opera) return false;
+    //    if (document.all && !window.opera) return false;
     var id = section + 'Link';
     YAHOO.util.Dom.setStyle(id,'color','red');
+}
+
+
+
+// Force IE5-6 to render transparent PNG images properly
+// correctPNG taken from from http://homepage.ntlworld.com/bobosola/pngtestfixed.htm
+function correctPNG() // correctly handle PNG transparency in Win IE 5.5 & 6.
+{
+    if (!isOldIE()) return false;
+   var arVersion = navigator.appVersion.split("MSIE")
+   var version = parseFloat(arVersion[1])
+   if ((version >= 5.5) && (document.body.filters))
+   {
+      for(var i=0; i<document.images.length; i++)
+      {
+         var img = document.images[i]
+         var imgName = img.src.toUpperCase()
+         if (imgName.substring(imgName.length-3, imgName.length) == "PNG")
+         {
+            var imgID = (img.id) ? "id='" + img.id + "' " : ""
+            var imgClass = (img.className) ? "class='" + img.className + "' " : ""
+            var imgTitle = (img.title) ? "title='" + img.title + "' " : "title='" + img.alt + "' "
+            var imgStyle = "display:inline-block;" + img.style.cssText
+            if (img.align == "left") imgStyle = "float:left;" + imgStyle
+            if (img.align == "right") imgStyle = "float:right;" + imgStyle
+            if (img.parentElement.href) imgStyle = "cursor:hand;" + imgStyle
+            var strNewHTML = "<span " + imgID + imgClass + imgTitle
+            + " style=\"" + "width:" + img.width + "px; height:" + img.height + "px;" + imgStyle + ";"
+            + "filter:progid:DXImageTransform.Microsoft.AlphaImageLoader"
+            + "(src=\'" + img.src + "\', sizingMethod='scale');\"></span>"
+            img.outerHTML = strNewHTML
+            i = i-1
+         }
+      }
+   }
+}
+
+
+// test for internet explorer (but not IE7)
+function isOldIE() {
+    if (navigator.appVersion.indexOf("MSIE") == -1) return false;
+    var temp=navigator.appVersion.split("MSIE");
+    return parseFloat(temp[1]) < 7;
 }
