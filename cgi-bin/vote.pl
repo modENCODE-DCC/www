e8e89213ee44f090708ed360eb84486a4e3d50b0
smckay <smckay>
2007-09-14 01:26:32 +0000

    Added file locking, cosmetic stuff

--- cgi-bin/vote.pl
+++ cgi-bin/vote.pl
@@ -8,19 +8,23 @@ use CGI::Pretty;
 # Dump fatal errors to the browser
 use CGI::Carp 'fatalsToBrowser';
 
-# Edit as required
-use constant TF_VOTES => "/FRONT-END/www/cgi-bin/tfvotes_test.txt";
+# For locking files while the script is executing
+use Fcntl ':flock';
 
-#use constant TF_VOTES => "/FRONT-END/www/cgi-bin/tfvotes.txt";
+# For generating unique IDs;
+use Digest::MD5 'md5_hex';
+
+# Edit as required
+use constant TF_VOTES => "/FRONT-END/www/cgi-bin/vote/tfvotes_test.txt";
+
+my $style = '<style>TD,TH {font-size:90%;}</style>';
 
 print header;
-print start_html(
-    -title   => 'Transcription Factor Priority Voting Page',
-    -style   => { 'src' => '/css/modencode.css' },
-    -bgcolor => 'white'
-);
-
-print start_form( -name => 'f1' );
+print start_html( 
+    -title => 'Transcription Factor Priority Voting Page',
+    -head => $style,
+    );
+print start_form(-name => 'f1');
 
 my $vote            = param('vote');
 my $usr_initial     = param('usr_initial') || '';
@@ -32,6 +36,7 @@ my $sty_pp          = param('sty_pp') || '';
 my $ab_avail        = param('ab_avail') || '';
 my $mut_avail       = param('mut_avail') || '';
 my $construct_avail = param('construct_avail') || '';
+my $create          = param('create');
 my $new             = 1
   if $usr_initial
   || $gene_name
@@ -40,14 +45,13 @@ my $new             = 1
   || $sty_pp
   || $ab_avail
   || $mut_avail;
-my $create = param('create');
 
 # Make sure we have at least a gene data or database ID
 if ( $new && !( $gene_name || $dbs_id ) ) {
     print h1(
         font(
             { color => 'red' },
-            'Either a gene name or database ID is required'
+            'Either a gene name, or a Worm/FlyBase ID is required'
         )
     );
     $new    = '';
@@ -55,35 +59,42 @@ if ( $new && !( $gene_name || $dbs_id ) ) {
 }
 
 my @tfvinfo = (
-    $usr_initial,     $gene_name, $dbs_id,   $tag_loc,
-    $tag,             $sty_pp,    $ab_avail, $mut_avail,
+    $usr_initial,     $gene_name, $dbs_id,   $tag,
+    $tag_loc,             $sty_pp,    $ab_avail, $mut_avail,
     $construct_avail, 1
 );
 for (@tfvinfo) {
     s/\t/ /g;
 }
 
-# If a new entry is requested, save it now
+# If a new entry is entered, save it now
+# but watch out for page re-loads
 if ($new) {
-    print "saving...", br;
-    open OUT, ">>" . TF_VOTES || die $!;
-    print OUT join( "\t", @tfvinfo ), "\n";
-    close OUT;
+    my $line = join( "\t", @tfvinfo );
+    my $file = TF_VOTES;
+    unless (`grep '$line' $file`) {
+      open OUT, ">>$file" || die $!;
+      flock(OUT, LOCK_EX); # exclisive file lock
+      print OUT "$line\n";
+      close OUT;
+    }
 }
 
 # Get the existing data
 open IN, TF_VOTES;
-my ( @vote_data, $vote_idx );
+flock(IN, LOCK_EX); # exclisive file lock
+
+my @vote_data;
 
 while ( my $line = <IN> ) {
     chomp $line;
     $line =~ /\S/ || next;
     my @columns = split "\t", $line;
     @columns == 10 || die "problem with data format for entry:\n$line\n";
-    $vote_idx++;
-    @columns[9]++ if $vote == $vote_idx;
+    my $vote_id = md5_hex(@columns[0..8]);
+    $columns[9]++ if $vote == $vote_id;
     push @columns,
-qq(<input type="radio" name="vote" value="$vote_idx" onclick="document.f1.submit()">);
+      qq(<input type="radio" name="vote" value="$vote_id" onclick="document.f1.submit()">);
     push @vote_data, \@columns;
 }
 close IN;
@@ -92,8 +103,8 @@ my @fields = (
     textfield( -name => 'usr_initial',     -size => 4,  -value => '' ),
     textfield( -name => 'gene_name',       -size => 8,  -value => '' ),
     textfield( -name => 'database_id',     -size => 8,  -value => '' ),
-    textfield( -name => 'tag_loc',         -size => 3,  -value => '' ),
     textfield( -name => 'tag',             -size => 5,  -value => '' ),
+    popup_menu( -name => 'tag_loc',        -values => ['','C','N']),
     textfield( -name => 'sty_pp',          -size => 15, -value => '' ),
     textfield( -name => 'ab_avail',        -size => 8,  -value => '' ),
     textfield( -name => 'mut_avail',       -size => 8,  -value => '' ),
@@ -124,9 +135,13 @@ my $new_entry = $create
     )
   );
 
+my $row_color = 'gainsboro';
 print start_table( { -border => 1, -width => '100%', -cellpadding => 2 } );
-print Tr( th( \@tfvheader ) );
-print map { Tr( td($_) ) } @vote_data;
+print Tr( {-bgcolor => 'lightblue'}, th( \@tfvheader ) );
+for my $row (@vote_data) {
+  $row_color = $row_color eq 'ivory' ? 'gainsboro' : 'ivory';
+  print Tr({-bgcolor=>$row_color}, td($row) )
+}  
 print $new_entry;
 print end_table;
 
@@ -134,7 +149,11 @@ print br, submit( -name => 'Update' ), end_form;
 
 # Store Final result here
 open OUT, ">" . TF_VOTES || die $!;
+flock(OUT, LOCK_EX);
 for (@vote_data) {
     print OUT join( "\t", @{$_}[ 0 .. 9 ] ), "\n";
 }
 close OUT;
+
+exit 0;
+
