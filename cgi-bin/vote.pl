aa064c6e85fb93b6046267bf472ef77bf291e497
zhengzha <zhengzha>
2007-09-27 18:42:18 +0000

    add email column

--- cgi-bin/vote.pl
+++ cgi-bin/vote.pl
@@ -38,6 +38,7 @@ print start_form(-name => 'f1');
 
 my $vote            = param('vote');
 my $usr_initial     = param('usr_initial') || '';
+my $email           = param('email') || '';
 my $gene_name       = param('gene_name') || '';
 my $dbs_id          = param('database_id') || '';
 my $tag             = param('tag') || '';
@@ -49,6 +50,7 @@ my $construct_avail = param('construct_avail') || '';
 my $create          = param('create');
 my $new             = 1
   if $usr_initial
+  || $email
   || $gene_name
   || $dbs_id
   || $tag
@@ -69,7 +71,7 @@ if ( $new && !( $gene_name || $dbs_id ) ) {
 }
 
 my @tfvinfo = (
-    $usr_initial, $gene_name, $dbs_id, $tag,
+    $usr_initial, $email, $gene_name, $dbs_id, $tag,
     $tag_loc, $sty_pp, $ab_avail, $mut_avail,
     $construct_avail, 1
 );
@@ -102,9 +104,9 @@ while ( my $line = <IN> ) {
     chomp $line;
     $line =~ /\S/ || next;
     my @columns = split "\t", $line;
-    #@columns == 11 || die "problem with data format for entry:\n$line\n";
-    my $vote_id = md5_hex(@columns[0..8]);
-    my $voters = pop @columns || '' if @columns == 11;
+    #@columns == 12 || die "problem with data format for entry:\n$line\n";
+    my $vote_id = md5_hex(@columns[0..9]);
+    my $voters = pop @columns || '' if @columns == 12;
     my @voters = split ',', $voters;
 
     # increment the vote and keep track of who voted
@@ -113,13 +115,13 @@ while ( my $line = <IN> ) {
         if (!$override && $voter && grep /$voter/, @voters) {
             print h4(font( {-color => 'red'},
                      "Sorry, someone at $voter has already voted for ". 
-			   ($columns[1] || $columns[2])), '&nbsp;&nbsp;',  
+			   ($columns[2] || $columns[3])), '&nbsp;&nbsp;',  
                   a({-href => url()."?vote=$vote;override=1"},'[Vote anyway]'),
 		  '&nbsp;',
 		  a({-href => url()}, '[Cancel]'));
         }
         else {
-            $columns[9]++;
+            $columns[10]++;
             $voters .= $voters ? ",$voter" : $voter;
         }
     }
@@ -133,6 +135,7 @@ close IN;
 
 my @fields = (
     textfield( -name => 'usr_initial',     -size => 4,   -value => '' ),
+    textfield( -name => 'email',           -size => 8,   -value => '' ),
     textfield( -name => 'gene_name',       -size => 8,   -value => '' ),
     textfield( -name => 'database_id',     -size => 10,  -value => '' ),
     textfield( -name => 'tag',             -size => 5,   -value => '' ),
@@ -146,7 +149,7 @@ my @fields = (
 my $new_vote = hidden( -name => 'votes', -value => 1, -disabled => 1, -size => 2 );
 
 my @tfvheader = (
-    "Initials",                "Gene<br>Name",
+    "Your<br>Name", "email",   "Gene<br>Name",
     "Worm/<br>FlyBase ID",     "Preferred<br>Tag ",
     "Preferred<br>Terminus",   "Study<br>Purpose",
     "Antibodies<br>available", "Mutants<br>available",
@@ -158,10 +161,10 @@ my @tfvheader = (
 my $submit = td( { -colspan => 2 }, submit( -name => 'Update' ));
 
 my $new_entry = $create
-  ? Tr( td( \@fields ).$submit )
+  ? Tr( td( \@fields ). $submit )
   : Tr(
     td(
-        { -colspan => 9 },
+        { -colspan => 10 },
         checkbox(
             -name    => 'create',
             -label   => '',
@@ -176,7 +179,7 @@ print start_table( { -border => 1, -width => '100%', -cellpadding => 2 } );
 print Tr( {-bgcolor => 'lightblue'}, th( \@tfvheader ) );
 for my $row (@vote_data) {
   $row_color = $row_color eq 'ivory' ? 'gainsboro' : 'ivory';
-  print Tr({-bgcolor=>$row_color}, td([@{$row}[0..10]]) )
+  print Tr({-bgcolor=>$row_color}, td([@{$row}[0..11]]) )
 }  
 print $new_entry, end_table, end_form;;
 
@@ -184,7 +187,7 @@ print $new_entry, end_table, end_form;;
 open OUT, ">" . TF_VOTES || die $!;
 flock(OUT, LOCK_EX);
 for (@vote_data) {
-    print OUT join( "\t", @{$_}[ 0 .. 9, 11]), "\n";
+    print OUT join( "\t", @{$_}[ 0 .. 10, 12]), "\n";
 }
 close OUT;
 
