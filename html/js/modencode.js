feb4ec140de8e2d088e151050dafa112d1ccd1bf
smckay <smckay>
2007-11-26 12:44:41 +0000

    removed old comment

--- html/js/modencode.js
+++ html/js/modencode.js
@@ -2,9 +2,6 @@
 if (isOldIE()) window.attachEvent("onload", correctPNG);
 
 function setActiveLink(section) {
-    /* currently disabled for IE until I can
-       get access to windows */
-    //    if (document.all && !window.opera) return false;
     var id = section + 'Link';
     YAHOO.util.Dom.setStyle(id,'color','red');
 }
